72276f979fd0332dae866793749486bb40b1151d
mohsenrs <mohsen8022@gmail.com>
2021-07-23 18:20:23 +0430

    async

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="home">
-    <PostList :posts="posts" />
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity';
 import PostList from '../components/PostList.vue';
+
 export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([
-      { title: 'soosis', body: 'lorem ipsum', id: 1 },
-      { title: 'soosis22', body: 'lorem ipsum', id: 2 },
-    ]);
-    return { posts };
+    const posts = ref([]);
+    const error = ref(null);
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts');
+        if (!data.ok) {
+          throw Error('no data available!');
+        }
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.log(error.value);
+      }
+    };
+    load();
+
+    return { posts, error };
   },
 };
 </script>
